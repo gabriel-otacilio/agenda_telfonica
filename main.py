b80838ebf408ed8550ef04e75d442a1cf7ed0441
gabriel <gabriel.otaciliodev@gmail.com>
2025-08-24 16:49:28 -0300

    adicionando menu

---
+++ main.py
@@ -0,0 +1,12 @@
+def menu():
+    print("1.adicionar contato")
+    print("2.remover contato")
+    print("3.editar contato")
+    print("4.listar contatos")
+    print("5.sair")
+
+
+while True:
+    menu()
+    entrada = input("escolha uma opção")
+
